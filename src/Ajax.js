85f1e74383aaa7e63b107475408f350b2ca3f14a
Sunit Kulkarni <sunitkulkarni@Sunits-MBP-2.direcpath.net>
2017-12-12 00:47:04 -0500

    added new memory game files

---
+++ src/Ajax.js
@@ -0,0 +1,19 @@
+import React, { Component }from 'react';
+
+class Ajax extends Component {
+	constructor () {
+		super();
+
+	}
+
+	render () {
+		return (
+			<div>
+				<h1>Ajax</h1>
+			</div>
+		);
+	}
+
+}
+
+export default Ajax;
